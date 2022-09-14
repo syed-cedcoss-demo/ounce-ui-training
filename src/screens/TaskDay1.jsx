777bf90da6e68350bc16f28cfa5a54d1c374f51b
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2022-09-14 15:18:43 +0530

    taoday 12-09-2022 task

---
+++ src/screens/TaskDay1.jsx
@@ -0,0 +1,12 @@
+import BasicForm from "../components/DayOneTask/BasicForm";
+import "../components/DayOneTask/BasicForm.css";
+
+const TaskDay1 = () => {
+  return (
+    <div className="main-container">
+      <BasicForm />
+    </div>
+  );
+};
+
+export default TaskDay1;
