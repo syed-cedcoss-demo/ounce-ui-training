b0518b0685d7f25e95bf1ab1c29e9fab4a47eb9b
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2022-09-15 14:33:03 +0530

    working on Taxonomy api with dropdown

--- src/components/DayTwoTask/TaxonomyListSelect.jsx
+++ src/components/DayTwoTask/TaxonomyListSelect.jsx
@@ -1,15 +1,16 @@
-import { Button, Card, List, TextStyles } from "@cedcommerce/ounce-ui";
+import { Card, Select } from "@cedcommerce/ounce-ui";
 import { useState } from "react";
 import { useEffect } from "react";
 import allData from "./data";
 
 const TaxonomyListSelect = () => {
   const [data, setData] = useState();
-  const [fLavel, setFLevel] = useState();
-  const [sLavel, setSLevel] = useState();
+  const [fLevel, setFLevel] = useState();
+  const [sLevel, setSLevel] = useState();
+  const [lbl1, setLbl1] = useState();
   const [lbl2, setLbl2] = useState();
-  const [tLavel, setTLevel] = useState();
-  const [lbl3, setLbl3] = useState();
+  const [tLevel, setTLevel] = useState();
+  //   const [lbl3, setLbl3] = useState();
 
   useEffect(() => {
     const level = [];
@@ -17,113 +18,107 @@ const TaxonomyListSelect = () => {
     setData(formatData);
     for (const fEl of formatData) {
       if (!fEl.includes(">")) {
-        level.push(fEl);
+        level.push({
+          label: fEl,
+          value: fEl,
+        });
       }
     }
     setFLevel(level);
   }, []);
 
   //handling data for second level list
-  const handleSecond = (lbl) => {
-    const sCheck = [];
+  const handleSecondLevel = (lbl) => {
+    console.log("lbl", lbl);
+    const check = [];
     for (const sEl of data) {
       if (sEl.includes(lbl) && sEl.includes(">")) {
         const arr = sEl.trim().split(">");
-        const found = sCheck.some((el) => el[1] === arr[1]);
+        const found = check.some((el) => el?.label === arr[1]);
         if (!found) {
-          sCheck.push(arr);
+          check.push({
+            label: arr[1],
+            value: arr[1],
+          });
+        }
+      }
+    }
+    setLbl1(lbl);
+    setSLevel(check);
+  };
+
+  //handling data for third level list
+  const handleThirdLevel = (lbl) => {
+    const check = [];
+    for (const sEl of data) {
+      if (sEl.includes(lbl) && sEl.includes(">")) {
+        const arr = sEl.trim().split(">");
+        const found = check.some((el) => el?.label === arr[2]);
+        if (!found) {
+          check.push({
+            label: arr[2],
+            value: arr[2],
+          });
         }
       }
     }
     setLbl2(lbl);
-    setSLevel(sCheck);
+    setTLevel(check);
   };
+  const optionsLbl1 = [
+    {
+      group: fLevel,
+      label: "Select your top level category",
+      value: "0",
+    },
+  ];
+  const optionsLbl2 = [
+    {
+      group: sLevel,
+      label: "Select your mid level category",
+      value: "0",
+    },
+  ];
+  const optionsLbl3 = [
+    {
+      group: tLevel,
+      label: "Select your mid level category",
+      value: "0",
+    },
+  ];
 
-  //listing third level
-  const handleThirdLevel = (lbl) => {
-    const sCheck = [];
-    for (const sEl of data) {
-      if (sEl.includes(lbl) && sEl.includes(">")) {
-        const arr = sEl.trim().split(">");
-        const found = sCheck.some((el) => el[2] === arr[2]);
-        console.log("found", found);
-        if (!found) {
-          sCheck.push(arr);
-        }
-      }
-    }
-    console.log("sCheck", sCheck);
-    setLbl3(lbl);
-    setTLevel(sCheck);
-  };
-  console.log("tLavel", tLavel);
-  // useEffect(() => {
-  //   var requestOptions = {
-  //     method: "GET",
-  //     redirect: "follow",
-  //   };
-
-  //   fetch(
-  //     "https://www.google.com/basepages/producttype/taxonomy.en-US.txt",
-  //     requestOptions
-  //   )
-  //     .then((response) => response.text())
-  //     .then((result) => {
-  //       // console.log("result", result);
-  //       const line = result.split("\n");
-  //       console.log("line", line);
-  //     })
-  //     .catch((error) => console.log("error", error));
-  // }, []);
   return (
     <div>
-      <h1>
-        <Card cardType="Bordered">
-          <List type="disc">
-            {fLavel?.length > 0 &&
-              fLavel.map((el, i) => {
-                return (
-                  <TextStyles textcolor="light" key={i}>
-                    <Button onClick={() => handleSecond(el)}>{el}</Button>
-                    {
-                      <List type="disc">
-                        {sLavel?.length > 0 &&
-                          lbl2 === el &&
-                          sLavel.map((sEl, j) => {
-                            return (
-                              <TextStyles textcolor="light" key={j}>
-                                {sEl?.length > 2 ? (
-                                  <Button
-                                    onClick={() => handleThirdLevel(sEl[1])}
-                                  >
-                                    {sEl[1]}
-                                  </Button>
-                                ) : (
-                                  sEl[1]
-                                )}
-                                {lbl3 === sEl[1] && (
-                                  <List type="disc">
-                                    {tLavel?.length &&
-                                      tLavel.map((tEl, k) => {
-                                        return (
-                                          <TextStyles key={k}>
-                                            {tEl[2]}
-                                          </TextStyles>
-                                        );
-                                      })}
-                                  </List>
-                                )}
-                              </TextStyles>
-                            );
-                          })}
-                      </List>
-                    }
-                  </TextStyles>
-                );
-              })}
-          </List>
-        </Card>
-      </h1>
+      <Card>
+        <Select
+          dropDownheight={600}
+          name="Select top level"
+          onChange={(e) => handleSecondLevel(e)}
+          options={optionsLbl1}
+          required
+          value={lbl1}
+        />
+        {sLevel?.length && (
+          <Select
+            dropDownheight={600}
+            name="Select mid level"
+            onChange={(e) => handleThirdLevel(e)}
+            options={optionsLbl2}
+            required
+            value={lbl2}
+          />
+        )}
+        {tLevel?.length && (
+          <Select
+            dropDownheight={600}
+            name="Select low level"
+            // onChange={(e) => handleThirdLevel(e)}
+            options={optionsLbl3}
+            required
+            value={lbl2}
+          />
+        )}
+      </Card>
     </div>
   );
 };
